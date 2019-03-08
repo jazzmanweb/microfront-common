fe4022cfb0c5f8f12323deb9b96aa46ee82a3bd6
Maciej Zoltowski <maciej.zoltowski@e-qsg.com>
2019-03-08 09:05:05 +0100

    fixes

---
+++ model/hero.interface.ts
@@ -0,0 +1,6 @@
+import {HeroTypeEnum} from './hero-type.enum';
+
+export interface HeroInterface {
+    name: string;
+    type: HeroTypeEnum;
+}
--- model/hero.model.ts
+++ model/hero.model.ts
@@ -1,4 +1,13 @@
-export class HeroModel {
-    name: string;
-    type: HeroModel;
+import {HeroTypeEnum} from './hero-type.enum';
+import {HeroInterface} from './hero.interface';
+
+export class HeroModel implements Required<HeroInterface> {
+    public name: string;
+    public type: HeroTypeEnum;
+
+    constructor(options?: Partial<HeroInterface>) {
+        options = options || {};
+        this.name = options.name || null;
+        this.type = options.type || null;
+    }
 }
